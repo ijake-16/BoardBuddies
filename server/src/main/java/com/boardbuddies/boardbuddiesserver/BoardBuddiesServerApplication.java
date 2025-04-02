d33e04162a1558a35652084d5dc9beeb7499b693
bestlalala <bestdustn@sookmyung.ac.kr>
2025-04-02 17:32:13 +0900

    setup: Initialize Spring Boot server project with Gradle setup
    - 초기 데이터베이스: h2
    - java 17

---
+++ server/src/main/java/com/boardbuddies/boardbuddiesserver/BoardBuddiesServerApplication.java
@@ -0,0 +1,13 @@
+package com.boardbuddies.boardbuddiesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBuddiesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardBuddiesServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/boardbuddies/boardbuddiesserver/BoardBuddiesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardbuddies.boardbuddiesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBuddiesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
